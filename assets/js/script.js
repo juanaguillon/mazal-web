e682af58bbb8c109e0b9912f019cf674d8a3b3c9
Cristian B <cristian@gmail.com>
2020-01-20 16:23:21 -0500

    rediseño wishlist

--- assets/js/script.js
+++ assets/js/script.js
@@ -932,3 +932,16 @@ $window.on("load resize", function() {
   toggleClassToMenuInResponse();
   changeTheHeaderWhenScrolling();
 });
+
+$(document).ready(function(){
+  $('.count').prop('disabled', true);
+   $(document).on('click','.plus',function(){
+  $('.count').val(parseInt($('.count').val()) + 1 );
+  });
+    $(document).on('click','.minus',function(){
+    $('.count').val(parseInt($('.count').val()) - 1 );
+      if ($('.count').val() == 0) {
+      $('.count').val(1);
+    }
+      });
+});
